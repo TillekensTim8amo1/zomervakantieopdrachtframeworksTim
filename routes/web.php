8f0a4b1dfcd6925b77db0acf5c0d2f19eb8ba3fd
Timtohy <1043001@mborijnland.nl>
2020-08-23 14:20:49 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/hello', function(){
+    $variable = 'Hello from Routes.';
+
+    return view('subviews.hello');
+});
